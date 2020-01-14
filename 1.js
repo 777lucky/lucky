c15a2dfb94378a81c30d259b4fa6e1e6daf8c9e8
777lucky <mr.lalintong@gmail.com>
2020-01-14 14:47:35 +0700

    [lucky]tambah fungsi trapezium

---
+++ 1.js
@@ -0,0 +1,18 @@
+function luasPersegi(s) {
+    console.log("Sisi", s);
+    console.log("Luas Persegi", s*s);
+}
+
+function luasSegiTiga(a,t) {
+    console.log("Alas", a)
+    console.log("Tinggi, t")
+    console.log("Luas Segitiga", a*t/2)
+}
+
+luasPersegi(5)
+luasSegiTiga(10,5)
+
+function luasTrapezium(s,t) {
+    console.log("Sisi", s);
+    console.log("Tinggi", t)
+}
